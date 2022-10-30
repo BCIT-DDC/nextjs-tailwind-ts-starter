6db6f3fb15146098885a7dec3a1bb7ea6b8bebba
Jamie Samuel <jsam07@hotmail.com>
2022-10-30 04:08:39 -0700

    Add normalize.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '@styles/Home.module.css';
+import styles from '@/styles/Home.module.css';
 
 export default function Home(): JSX.Element {
     return (
@@ -26,7 +26,7 @@ export default function Home(): JSX.Element {
 
                 <div className={styles.grid}>
                     <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h2>Documentation &rarr;</h2>
+                        <h2 className="text-red-500">Documentation &rarr;</h2>
                         <p>
                             Find in-depth information about Next.js features and
                             API.
